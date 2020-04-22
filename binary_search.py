20eb72b37ca8c56f57c3a41d393e23f9dab97e56
rwik19 <62844629+rwik19@users.noreply.github.com>
2020-04-22 22:34:38 +0530

    Add file

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,21 @@
-def main():
-    arr = [1,2,3,4,5,6,7,8]
-    value = int(input("Enter a value to be searched: "))
-    print(binarySearch(arr, value))
+#to search a value in a SORTED(increasing order) list.
+#if list name is values and you are searching for x, use binarySearch(values, x)
 
 def binarySearch(arr, value):
+    
+    #find the middle of the list
     m = len(arr)//2
+
+    #value found
     if value == arr[m]:
         return True
+    
+    #list exhausted. value not found
     if len(arr)==1:
         return False
     if value<arr[m]:
+        #if value less than middle value, binary search the left
         return binarySearch(arr[:m], value)
     if value>arr[m]:
+        #if value greater than middle value, binary search the right
         return binarySearch(arr[m+1:], value)
-
-main()
